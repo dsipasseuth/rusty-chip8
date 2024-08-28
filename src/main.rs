5a95ec01d6e46adbb90533128d06c11c2d71ee6d
Daniel S. <sipasseuth.daniel@gmail.com>
2024-08-28 11:30:59 +0100

    fixes issues with display

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,7 @@ use std::{
     io::{self, stdout, Stdout},
     time::{Duration, Instant},
 };
+use ratatui::layout::Flex;
 
 fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
@@ -45,10 +46,11 @@ fn main() -> io::Result<()> {
         let _ = terminal.draw(|frame| {
             if vm.draw_flag {
                 let vertical_layout =
-                    Layout::vertical([Constraint::Percentage(90), Constraint::Percentage(10)]);
+                    Layout::vertical([Constraint::Percentage(80), Constraint::Percentage(20)]);
                 let [top, bottom] = vertical_layout.areas(frame.area());
-                frame.render_widget(as_canvas(&vm), top);
-                frame.render_widget(as_instruction(), bottom);
+                let centered_top = center(top, Constraint::Length(64), Constraint::Length(32));
+                frame.render_widget(as_canvas(&vm), centered_top);
+                frame.render_widget(as_instruction(&vm), bottom);
             }
         });
 
@@ -67,19 +69,23 @@ fn main() -> io::Result<()> {
     restore_terminal()
 }
 
+///
+/// Returns points in the canvas screen referential.
+/// Chip8 have a top left coordinates being (0,0),
+/// while ratatui works with the bottom left coordinates being (0,0)
+///
 fn as_points(vm: &Chip8) -> Vec<(f64, f64)> {
-    let mut y_axis = 0f64;
-    let mut x_axis = 0f64;
-    let mut breakline = 0;
+    let mut y_axis = 32;
+    let mut x_axis = 0;
     let mut coords = vec![];
     for pixel in vm.gfx {
         if pixel {
-            coords.push((x_axis, y_axis))
+            coords.push((x_axis as f64, y_axis as f64))
         }
-        breakline += 1;
-        if breakline % 64 == 0 {
-            y_axis += 1.0;
-            x_axis = 0f64;
+        x_axis += 1;
+        if x_axis % 64 == 0 {
+            y_axis -= 1;
+            x_axis = 0;
         }
     }
     coords
@@ -100,8 +106,10 @@ fn as_canvas(vm: &Chip8) -> impl Widget {
         })
 }
 
-fn as_instruction() -> impl Widget {
+fn as_instruction(vm: &Chip8) -> impl Widget {
+    let coords = as_points(&vm);
     Paragraph::new(format!("Press 'p' to quit."))
+        .block(Block::bordered().title("Instructions"))
         .black()
         .on_white()
 }
@@ -116,4 +124,11 @@ fn restore_terminal() -> io::Result<()> {
     disable_raw_mode()?;
     stdout().execute(LeaveAlternateScreen)?;
     Ok(())
+}
+fn center(area: Rect, horizontal: Constraint, vertical: Constraint) -> Rect {
+    let [area] = Layout::horizontal([horizontal])
+        .flex(Flex::Center)
+        .areas(area);
+    let [area] = Layout::vertical([vertical]).flex(Flex::Center).areas(area);
+    area
 }
